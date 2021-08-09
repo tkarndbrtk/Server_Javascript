fd7fa6f766e393b5ae06f4ba802ee9ca80370001
tkarndbrtk <tkarndbrtk@naver.com>
2021-08-09 23:28:15 +0900

    java 입문

---
+++ 100분완성/variable.js
@@ -0,0 +1,11 @@
+let a = "x";
+let b = "c";
+
+
+// 자바에서 변수 선언시, 상수 : const
+//                  변수 : let
+// let a = "y" --> 오류발생
+
+a = "hello";
+console.log(a);
+// hello //
---
+++ 100분완성/자료형.js
@@ -0,0 +1,12 @@
+const name = "Mike";
+const age = 30;
+
+const name1 = 'Mike'
+const name2 = "Mike"
+const name3 = '엑띡 없음'
+
+const x = 1/0
+console.log(x)
+// infinity //
+
+console.log(name == 'Mike')
