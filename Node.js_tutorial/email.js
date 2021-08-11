e9e8147965372d94bcc68f04b214609b35cade0b
tkarndbrtk <tkarndbrtk@naver.com>
2021-08-12 03:33:53 +0900

    1hour node.JS

--- Node.js_tutorial/email.js
+++ Node.js_tutorial/email.js
@@ -1,10 +1,10 @@
-// let a = 3;
-// let b = 5;
-// let c = a + b
-// console.log("c", c)
 
-const nodemailer =  require('nodemailer');
+
+
+const nodemailer =  require('nodemailer'); // nodemailer 선언
+// email 계정정보
 const email = {
+// 이 부분은 mailtrap.io 에서 퍼온 코드
   host: "smtp.mailtrap.io",
   port: 2525,
   auth: {
@@ -14,11 +14,13 @@ const email = {
 
 }
 const send = async (option) =>{
+  // 이메일 보내는 함수 선언 , 마찬가지로 퍼옴 
   nodemailer.createTransport(email).sendMail(option, (error, info) => {
     if(error) {
       console.log(error);
     }else {
       console.log(info);
+    // sendMail메소드 이용, 콜백함수를 만들어 에러발생시 콘솔 출력
       return info.response;
     }
   })
@@ -32,3 +34,4 @@ let email_data = {
 }
 
 send(email_data)
+// 이메일 보내기
--- Node.js_tutorial/server.js
+++ Node.js_tutorial/server.js
@@ -5,12 +5,13 @@ const server = app.listen(3000, () => {
   console.log('Start server: localhost:3000')
 });
 
-var mysql = require('mysql');
+var mysql = require('mysql'); //mysql 선언 후 mysql모듈 가져오기
+//DB와 연결하기위해 DB정보를 입력하는 코드를 npm에서 퍼왔다.
 var pool  = mysql.createPool({
   connectionLimit : 10,
   host            : 'example.org',
   user            : 'bob',
-  password        : 'secret',
+  password        : 'secret', 
   database        : 'my_db'
 });
 
@@ -27,17 +28,17 @@ app.get('/about', function (req, res) {
 });
 
 
-
+// 라우터를 하나 가져왔음 -> DB pool을 받고 connection을 연결
 app.get('/db ', function (req, res) {
   pool.getConnection(function(err, connection) {
-    if (err) throw err; // not connected!
+    if (err) throw err; // 에러 뽑기
    
-    // Use the connection
+    // 연결시 query를 날린다 -> Test table은 마리아DB의 모든걸 가져온다.
     connection.query('select * from Test', function (error, results, fields) {
-      res.send(JSON.stringify(results));
-      console.log('results:', results);
-      // When done with the connection, release it.`
-      connection.release();
+      res.send(JSON.stringify(results)); // table Data가져오면 화면에 가져온 Data를 보낸다.
+      console.log('results:', results); // 가져왔는지 메시지 띄우기.
+      
+      connection.release(); // 연결시 출력
    
       // Handle error after the release.
       if (error) throw error;
